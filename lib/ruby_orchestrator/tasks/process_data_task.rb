4d33d34102137aba2851541cf0e46f66682f83e8
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-21 20:43:29 -0300

    Update process_data_task.rb

--- lib/ruby_orchestrator/tasks/process_data_task.rb
+++ lib/ruby_orchestrator/tasks/process_data_task.rb
@@ -1,1 +1,25 @@
+module RubyOrchestrator
+    module Tasks
+        class ProcessDataTask < RubyOrchestrator::Core::BaseTask
+            def perform(context)
+        puts "[TASK: ProcessData] -> Transformando em dados brutos.."
+
+
+        raw = context[:api_response][:raw_payload]
+
+        parts = raw.split(';')
+        processed = {}
+        parts.each do |part|
+            key, value = part.split('=')
+            processed[key.to_sym] = value
+        end
+     
+
+        context[:processed_info] = processed
+        context[:status] = "COMPLETED"
+        puts "[Task: ProcessData] -> Transformação concluída: #{processed.inspect}"
+       end
+    end
+   end
+end
 
