54c5a5d7cd8e290885731f901b09f253ad78bd64
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-21 20:42:06 -0300

    Update context.rb

--- lib/ruby_orchestrator/core/context.rb
+++ lib/ruby_orchestrator/core/context.rb
@@ -1,1 +1,21 @@
+module RubyOrchestrator
+    module Core
+        class Context
+            def initialize
+                @data = {}
+            end
 
+            def []=(key, value)
+            @data[key] = value
+            end
+
+            def[](key)
+              @data[key]
+            end
+
+            def inspect
+                @data.inspect
+            end
+        end
+    end
+end
