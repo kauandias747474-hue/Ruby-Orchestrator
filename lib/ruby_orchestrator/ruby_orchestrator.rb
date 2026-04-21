19b6bec1d1bd894f9d50e859c2f6090d440299fa
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-21 20:44:04 -0300

    Create ruby_orchestrator.rb

---
+++ lib/ruby_orchestrator/ruby_orchestrator.rb
@@ -0,0 +1,9 @@
+require_relative 'ruby_orchestrator/core/context'
+require_relative 'ruby_orchestrator/core/base_task' 
+require_relative  'ruby_orchestrator/core/engine'
+
+module RubyOrchestrator
+
+    VERSION = "1.0.0"
+
+end
