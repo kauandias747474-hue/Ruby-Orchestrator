40a774b628a7a734f1340988ecad9458b67c738f
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-21 20:43:15 -0300

    Update fetch_api_task.rb

--- lib/ruby_orchestrator/tasks/fetch_api_task.rb
+++ lib/ruby_orchestrator/tasks/fetch_api_task.rb
@@ -1,1 +1,21 @@
+module RubyOrchestrator
+    module Tasks
+        class FetchApiTask < RubyOrchestrator::Core::BaseTask
+          def perform(context)
+        puts "[Task: FetchApi] -> Buscando dados da API externa..."
+        
+        response_data = { 
+          id: 101, 
+          raw_payload: "sensor_id=A1;temp=22.5;status=ok",
+          provider: "WeatherAPI"
+        }
+
+        
+     context[:api_response] = response_data
+        context[:status] = "DATA_FETCHED"
+        puts "[Task: FetchApi] -> Dados armazenados no contexto."
+      end
+    end
+  end
+end
 
