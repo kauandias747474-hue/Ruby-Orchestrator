3f8f229f764772da11533a4af5eca31f11ae3448
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-21 20:44:22 -0300

    Update engine_spec.rb

--- spec/engine_spec.rb
+++ spec/engine_spec.rb
@@ -1,1 +1,21 @@
 
+require 'spec_helper'
+
+
+RSpec.describe RubyOrchestrator::Core::Engine do 
+    let(:engine) { described_class.new }
+
+    class FakeTask < RubyOrchestrator::Core::BaseTask
+        def  perform(context); context[:worked] = true; end
+    end
+
+    it "deve executar uma tarefa adicionada com sucesso" do
+        engine.add_task(FakeTask)
+        engine.run!
+        expect(engine.instance_variable_get(:@context)[:worked]).to be true
+    end
+
+    it "deve respeitar o número de retries em caso de falha" do
+
+    end
+end 
